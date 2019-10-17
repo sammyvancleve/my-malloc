54e419492f994864a4409c71c22bdfcd03878848
Sammy Van Cleve <svancleve@middlebury.edu>
2019-10-17 15:51:55 -0400

    add struct to put at beginning of allocation, scaffolding

--- my-malloc.c
+++ my-malloc.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdint.h>
 
 int mymalloc(int bytes);
+void free(void *ptr);
+
+//8 bytes long
+struct alloc {
+    uint32_t nextAllocOffset;
+    uint32_t free;
+};
 
 int main(int argc, char *argv[]){
 
@@ -12,8 +20,19 @@ int main(int argc, char *argv[]){
 }
 
 int mymalloc(int bytes){
+    //check if it needs to sbrk more memory...
+    //check for empty space 
+    //check for allocation that has been freed
+    //if it does, then sbrk...
+    if (bytes % 1024 != 0){
+        bytes = bytes + (1024 - (bytes % 1024));
+    }
     int addr = sbrk(bytes);
     printf("%p\n", (void *)&addr);
 
     return addr;
+}
+
+void free(void *ptr){
+
 }
