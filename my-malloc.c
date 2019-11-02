f6f6fadb6bf391543c5fe80ed6623c431ce1e840
Sammy Van Cleve <svancleve@middlebury.edu>
2019-11-02 14:04:52 -0400

    stashing work to pull on other computer

--- my-malloc.c
+++ my-malloc.c
@@ -1,37 +1,54 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
-int mymalloc(int bytes);
+void *mymalloc(int bytes);
 void free(void *ptr);
+static void *allocstart;
 
-//8 bytes long
-struct alloc {
-    uint32_t nextAllocOffset;
-    uint32_t free;
+//16 bytes long
+struct allocation {
+    uint64_t nextallocoffset;
+    uint64_t free;
 };
 
 int main(int argc, char *argv[]){
+
     char *s;
-    s = mymalloc(10);
-    s = "poo\0";
+    s = mymalloc(15);
+    s = "Homer's BBBQ\0";
+    char *t;
+    t = mymalloc(50);
+    t = "The extra B is for BYOBB\0";
     printf("%s\n", s);
+    printf("%s\n", t);
 }
 
-int mymalloc(int bytes){
-    //check if it needs to sbrk more memory...
-    //check for empty space 
-    //check for allocation that has been freed
-    //if it does, then sbrk...
-    if (bytes % 1024 != 0){
-        bytes = bytes + (1024 - (bytes % 1024));
+void *mymalloc(int bytes){
+    if (allocstart == NULL){
+        allocstart = sbrk(0);
+        printf("allocstart %p\n", allocstart);
     }
-    int addr = sbrk(bytes);
-    //increment addr by however much space there needs
-    //  to be for alloc struct
-    printf("%p\n", (void *)&addr);
-
-    return addr;
+    void *addr;
+    //allocation.free = -1;
+    if (bytes % 16 != 0) {
+        bytes = bytes + (16 - (bytes % 16));
+    }
+    if((addr = sbrk(bytes)) != (void *) -1){
+        printf("%p\n", (void *)&addr); //testing purposes
+        //increment addr by however much space there needs
+        //  to be for alloc struct
+        void *newbreak = sbrk(0);
+        size_t breaksize = newbreak - addr;
+        memset(addr, 0, breaksize);
+        struct allocation *alloc = addr;
+        alloc->free = 0;
+        alloc->nextallocoffset = bytes + 16;
+        return addr;
+    } else {
+        return NULL;
+    }
 }
 
 void free(void *ptr){
