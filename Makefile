d827b10d2259b788b45d920bed732f22898aecb1
Sammy Van Cleve <svancleve@middlebury.edu>
2019-11-10 14:41:44 -0500

    change malloc so it adds bookkeeping struct for next alloc, change type of some variables, disable memset, update makefile so i have to type less -- all of this will change a lot in next few commits

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 my-malloc: my-malloc.c
-	gcc -Wall -pedantic -g -o my-malloc my-malloc.c
+	gcc -Wall -pedantic -g -rdynamic -shared -fPIC -o my-malloc.so my-malloc.c
 
+test-malloc: test-malloc.c
+	gcc -g -o test-malloc test-malloc.c
+
+.PHONY: testls
+
+testls:
+	gdb --args env LD_PRELOAD=./my-malloc.so ls
--- my-malloc.c
+++ my-malloc.c
@@ -3,11 +3,12 @@
 #include <stdint.h>
 #include <string.h>
 
-#define BOUNDARY 16
+#define BOUNDARY 16 
 #define BOOKKEEPING_SIZE 16
+#define SBRK_INCR 4096
 
-void *mymalloc(int bytes);
-void myfree(void *ptr);
+void *malloc(size_t bytes);
+void free(void *ptr);
 static void *allocstart;
 
 //16 bytes long
@@ -17,29 +18,28 @@ struct allocation {
 };
 
 int main(int argc, char *argv[]){
+/*
     char *s;
-    s = mymalloc(70);
-    printf("agh %p\n", s);
-    strcpy(s, "Homer's bbbq\0");
-    printf("agh %p\n", s);
+    s = malloc(10000);
+    strcpy(s, "Homer's BBBQ\0");
     printf("%s\n", s);
-    myfree(s);
+    free(s);
     char *t;
-    t = mymalloc(50);
-    strcpy(t, "The extra B is for BYOBB");
+    t = malloc(50);
+    strcpy(t, "The extra B is for BYOBBB\0");
     printf("%s\n", s);
-    printf("%s\n", t);
+    printf("%s\n", t);*/
 }
 
 //returns pointer of type void *
-void *mymalloc(int bytes){
+void *malloc(size_t bytes){
     void *currentbreak = sbrk(0);
     if (bytes % BOUNDARY != 0) {
         bytes = bytes + (BOUNDARY - (bytes % BOUNDARY));
     }
     if (allocstart != NULL){
         struct allocation *checkalloc = allocstart;
-        while (checkalloc < currentbreak){
+        while ((void *)checkalloc < currentbreak){
             if (((*checkalloc).free = 1) && (*checkalloc).offset > bytes + 16){
                 (*checkalloc).free = 0;
                 return (void *)checkalloc + BOOKKEEPING_SIZE;
@@ -49,23 +49,27 @@ void *mymalloc(int bytes){
         }
     }
     void *addr;
-    if((addr = sbrk(bytes)) != (void *) -1){
+    if((addr = sbrk(bytes*4)) != (void *) -1){
         if (allocstart == NULL){
             allocstart = addr;
         }
         void *newbreak = sbrk(0);
-        size_t breaksize = (int *)newbreak - (int *)addr;
-        memset(addr, 0, breaksize);
+        int breaksize = (int *)newbreak - (int *)addr;
+        //memset(addr, 0, breaksize);
         struct allocation *alloc = addr;
         (*alloc).free = 0;
         (*alloc).offset = bytes + BOOKKEEPING_SIZE;
+        struct allocation *nextalloc = (char *)addr + BOOKKEEPING_SIZE +
+        (*alloc).offset;
+        (*nextalloc).free = 1;
+        (*nextalloc).offset = NULL;
         return addr + BOOKKEEPING_SIZE;
     } else {
         return NULL;
     }
 }
 
-void myfree(void *ptr){
+void free(void *ptr){
     void *loc = ptr;
     uint64_t *free = loc-BOOKKEEPING_SIZE;
     *free = 1;
