add72a4745106ad42112894452faf8b032191e50
Sammy Van Cleve <svancleve@middlebury.edu>
2019-11-14 14:47:39 -0500

    improve makefile, some formatting changes in my-malloc, added readme

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
+CFLAGS=-Wall -pedantic -g -rdynamic -shared -fpic
+
 my-malloc: my-malloc.c
-	gcc -Wall -pedantic -g -rdynamic -shared -fPIC -o my-malloc.so my-malloc.c
+	gcc $(CFLAGS) -o my-malloc.so my-malloc.c
 
 test-malloc: test-malloc.c
 	gcc -g -o test-malloc test-malloc.c
 
 .PHONY: testls
-
 testls:
 	gdb --args env LD_PRELOAD=./my-malloc.so ls -l
+
+.PHONY: clean
+clean:
+	rm -f my-malloc.so
--- my-malloc.c
+++ my-malloc.c
@@ -4,11 +4,12 @@
 #include <string.h>
 
 #define BOUNDARY 16 
-#define BOOKKEEP_SIZE 16
+#define BOOK_SIZE 16
 
 void *malloc(size_t bytes);
 void free(void *ptr);
 void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
 static void *allocstart;
 
 //16 bytes long
@@ -25,20 +26,19 @@ void *malloc(size_t bytes){
         bytes = bytes + (BOUNDARY - (bytes % BOUNDARY));
     }
     if (allocstart != NULL) {
-        size_t allocsize = bytes + BOOKKEEP_SIZE;
+        size_t allocsize = bytes + BOOK_SIZE;
         struct allocation *checkalloc = allocstart;
         while ((void *)checkalloc < currentbreak) {
             if ((*checkalloc).free == 1) {
                 if ((*checkalloc).offset > allocsize) {
                     (*checkalloc).free = 0;
-                    addr = (char *)checkalloc + BOOKKEEP_SIZE;
+                    addr = (char *)checkalloc + BOOK_SIZE;
                     return addr;
                 }
                 else if ((int *)(*checkalloc).offset == NULL) {
                     if ((void *)(checkalloc + allocsize) < currentbreak) {
                         (*checkalloc).free = 0;
-                        addr = (char *)checkalloc + BOOKKEEP_SIZE;
-                        //TODO set offset properly?
+                        addr = (char *)checkalloc + BOOK_SIZE;
                         return addr;
                     } else {
                         break;
@@ -59,15 +59,15 @@ void *malloc(size_t bytes){
 
         struct allocation *alloc = addr;
         (*alloc).free = 0;
-        (*alloc).offset = bytes + BOOKKEEP_SIZE;
-        
-        void *nextallocaddr = (char *)addr + BOOKKEEP_SIZE +
-        (*alloc).offset;
+        (*alloc).offset = bytes + BOOK_SIZE;
+
+        void *nextallocaddr = (char *)addr + BOOK_SIZE +
+            (*alloc).offset;
         struct allocation *nextalloc = nextallocaddr;
         (*nextalloc).free = 1;
         (*nextalloc).offset = (uint64_t)NULL;
 
-        return (char *)addr + BOOKKEEP_SIZE;
+        return (char *)addr + BOOK_SIZE;
     } else {
         return NULL;
     }
@@ -75,7 +75,7 @@ void *malloc(size_t bytes){
 
 void free(void *ptr){
     if (ptr != NULL){
-        uint64_t *free = (uint64_t *)((char *)ptr-BOOKKEEP_SIZE);
+        uint64_t *free = (uint64_t *)((char *)ptr-BOOK_SIZE);
         *free = 1;
     }
 }
@@ -94,11 +94,11 @@ void *realloc(void *ptr, size_t size){
     if (size == 0){
         free(ptr);
     }
-    void *checkallocaddr = (char *)ptr - BOOKKEEP_SIZE;
+    void *checkallocaddr = (char *)ptr - BOOK_SIZE;
     struct allocation *checkalloc = checkallocaddr;
-    size_t currentsize = ((*checkalloc)).offset - BOOKKEEP_SIZE;
+    size_t currentsize = ((*checkalloc)).offset - BOOK_SIZE;
     checkalloc += (*checkalloc).offset;
-    while ((checkalloc < currentbreak) && ((*checkalloc).free == 1)) {
+    while (((void *)checkalloc < currentbreak) && ((*checkalloc).free == 1)) {
         currentsize += (*checkalloc).offset;
         if (currentsize >= size){
             return ptr;
